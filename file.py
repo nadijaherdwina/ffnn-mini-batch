1ba03b6b950ea1f1dd9facd45a7cb5b39a32fe3c
Nadija Herdwina <13516130@std.stei.itb.ac.id>
2019-09-27 19:25:56 +0700

    add load & split data function, init fit function

--- file.py
+++ file.py
@@ -1,41 +1,51 @@
 
 import numpy as np
+import pandas as pd
+from scipy.io import arff
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
+def load(filename):		
+	data = arff.loadarff(filename)
+	df = pd.DataFrame(data[0])
+	# Convert string attribute to integer
+	df.outlook = pd.Categorical(pd.factorize(df.outlook)[0])
+	df.outlook = pd.to_numeric(df.outlook, errors='coerce')
+	df.windy = pd.Categorical(pd.factorize(df.windy)[0])
+	df.windy = pd.to_numeric(df.windy, errors='coerce')
+	df.play = pd.Categorical(pd.factorize(df.play)[0])
+	df.play = pd.to_numeric(df.play, errors='coerce')
 
-# class Neuron:
-# 	def __init__(self, value):
-# 		self.value = value
-# 		self.weight = weight
+	df.head()
+	return df
+
+def split(data, test_size=0.2):
+	y = data.play
+	x = data.drop('play',axis=1)
+	scaler = preprocessing.MinMaxScaler().fit(x)
+	x = scaler.transform(x)
+	X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+	return X_train, X_test, y_train, y_test
 
 class FFNN: 
-	def __init__(self, nb_feature, nb_hidden_layer, nb_nodes, batch_size, epoch=10, momentum=0.0001, learning_rate=0.5):
+	def __init__(self, nb_feature, nb_hidden_layer, nb_nodes):
 		self.nb_output_layer = 1
 		self.nb_feature = nb_feature
 		self.nb_hidden_layer = nb_hidden_layer
 		self.nb_nodes = nb_nodes
 		self.nb_weight_layer = nb_hidden_layer + 1
-		self.learning_rate = learning_rate
-		self.momentum = momentum
-		self.batch_size = batch_size
-		self.epoch = epoch
 		
 		self.weights = []
 		self.output_neurons = []
 		self.feature_neurons = []
 		self.hidden_neurons = []
 
-	def softmax(self, x):
-		return 0 if x < 0 else x
-
 	def sigmoid(self, x):
 		return 1/(1 + np.exp(-x)) 
 
 	def count_error(self, target_output):
 		return pow(self.learning_rate - target_output)/2
-
-	def update_weight(self):
-		return 0
-		
+	
 	def init_weights(self):
 		for i in range (self.nb_weight_layer):
 			temp_weight = []
@@ -47,44 +57,53 @@ class FFNN:
 				temp_weight = np.random.randn(self.nb_nodes, self.nb_output_layer)
 			self.weights.append(temp_weight)
 	
-	def init_hidden_neurons(self):
-		#init array hidden, diisi 0 aja yang penting kebikin
-		for i in range(self.nb_hidden_layer):
-			temp_hidden_layer = []
-			for i in range(self.nb_nodes):
-				temp_hidden_layer.append(0)
-			self.hidden_neurons.append(temp_hidden_layer)
-
-	def init_feature_neurons(self):
-		# harusnya ambil nilai tiap fitur dari row yang lagi diitung
-		for i in range (self.nb_feature):
-			self.feature_neurons.append(0)
-	
-	def make_network(self):
-		self.init_weights()
-		self.init_feature_neurons()
-		self.init_hidden_neurons()
 
 	def get_sigmoid_value(self, m=None, n=None, output=False):
-		
 		if output:
 			m = self.nb_weight_layer-1
 			n = 0
-	
 		previous_layer = self.feature_neurons if m == 0 else self.hidden_neurons[m-1]
 		weight_layer = self.weights[m]
 		sigmoid_value = 0
-
+		
 		for i in range(len(previous_layer)):
 			sigmoid_value += previous_layer[i] * weight_layer[i, n]
-			print(m,i,n)
 		sigmoid_value = self.sigmoid(sigmoid_value)
-		# print('sigmoid_value= sigmoid(w',m, i, n, ' * h', m-1,i ,')')
 		return sigmoid_value
+
+	def get_output(self, input):
+		self.feature_neurons = []
+		for item in input:
+			self.feature_neurons.append(item)
+
+		for i in range(self.nb_hidden_layer):
+			temp_hidden_layer = []
+			for j in range(self.nb_nodes):
+				temp_hidden_layer.append(self.get_sigmoid_value(i, j))
+			self.hidden_neurons.append(temp_hidden_layer)
+		output = self.get_sigmoid_value(output=True)	
+		return output	
+
+	def fit(self, x_train, y_train, batch_size, momentum=0.001, learning_rate=0.5, epoch=5):
+		self.init_weights()
+		for i in range(epoch):
+			#masuk ke epoch
+			for	j in range(0, len(y_train), batch_size):
+				#masuk ke batch
+				x_mini = x_train[j:j+batch_size]
+				y_mini = y_train[j:j+batch_size]
+				x_output = []
+				for x in x_mini:
+					x_output.append(self.get_output(x))
+				
+				delta_x = []
+				# for x, y in x_mini, y_mini:
+					# itung selisih prediksi dgn ekspektasi
 
 
 if __name__ == "__main__":
-	ffnn = FFNN(2,2,3,5)
-	ffnn.make_network()
-	ffnn.get_sigmoid_value(output=True)
+	data = load('weather.arff')
+	X_train, X_test, y_train, y_test = split(data, 0.2)
+	ffnn = FFNN(X_train.shape[1],2,3)
+	ffnn.fit(X_train, y_train, 2)
         
